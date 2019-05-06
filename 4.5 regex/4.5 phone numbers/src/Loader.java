9eb1f26def94b0e863fa3b6f61591fd9cd49ed41
sofia_f <lx.sonik.xl@gmail.com>
2019-05-06 21:35:32 +0300

    working vesion

--- 4.5 regex/4.5 phone numbers/src/Loader.java
+++ 4.5 regex/4.5 phone numbers/src/Loader.java
@@ -1,19 +1,27 @@
-import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Loader {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        //String input = sc.nextLine();
-        String input = "71234567890";
-        double num = 33.0;
+        String input = sc.nextLine();
+        //String input = "+7 (962) 383 772 8";
 
         input = input.replaceAll("[^0-9]+", "");
 
-        DecimalFormat formatter = new DecimalFormat("+# ### ###-##-##");
+        //DecimalFormat formatter = new DecimalFormat("+# ### ###-##-##"); - это не работает! (увы)
 
-        if(input.length() == 11 && input.charAt(0) == '7'){
-            System.out.println(formatter.format(Long.parseLong(input)));
+        if(input.length() == 11 && (input.charAt(0) == '7' || input.charAt(0) == '8') && input.charAt(1) == '9'){
+            printRussianNumber10num(input.substring(1));
+        } else if (input.length() == 10 && input.charAt(0) == '9'){
+            printRussianNumber10num(input);
+        } else {
+            System.out.println("Некорректный номер");
         }
+
+
+    }
+
+    public static void printRussianNumber10num(String input){
+        System.out.println("+7 " + input.substring(0, 3) + " " + input.substring(3, 6) + "-" + input.substring(6, 8) + "-" + input.substring(8));
     }
 }
