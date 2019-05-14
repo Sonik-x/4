2a58421dd31801f1b0b1e8d0bc657ef9721b0784
sofia_f <lx.sonik.xl@gmail.com>
2019-05-14 15:20:30 +0300

    win

--- 4.5 regex/()4.5 check repeating chars/src/Loader.java
+++ 4.5 regex/()4.5 check repeating chars/src/Loader.java
@@ -1,53 +1,27 @@
+import java.util.HashSet;
+
 public class Loader {
     public static void main(String[] args) {
         String input = "";
-        for (int i = 0; i < 30000; i++) {     //так опытным путём выяснилось, что система может напечатать строку из символов до 8195 и не больше =)
+        for (int i = 0; i < 65536; i++) {     //так опытным путём выяснилось, что система может напечатать строку из символов до 8195 и не больше =)
             input += (char)i;                   //при этом программа запускается с любым числом, но после 65536 начинает отдавать true
         }
         System.out.println(System.currentTimeMillis()); //поскольку я сильно сомневаюсь, что кому-то в жизни может пригодиться такой алгоритм для проверки строк длиннее 30000 символов...
-        System.out.println(hasDuplicateChars(input));   //массив символов победил
+        System.out.println(hasDuplicateCharsHash(input));   //массив символов победил
         System.out.println(System.currentTimeMillis());
     }
 
-    public static boolean hasDuplicateSymbols(String string) {   //строка без повторений 1000 символов 19мс, 3000 20мс, 8195 30мс, 30000 191мс, 65536 794мс
-        for (int i = 0; i < string.length(); i++) {
-            for (int j = i + 1; j < string.length(); j++){
-                if(string.charAt(i) == string.charAt(j)){
-                    return true;
-                }
-            }
+
+    public static boolean hasDuplicateCharsHash(String string) {   //строка без повторений 1000 0,..мс, 3000 1мс, 8195 2мс, 30000 7мс, 65536 17мс
+        char[] chars = string.toCharArray();
+        HashSet<Character> charsSet = new HashSet<>();
+        for (char ch : chars){
+            charsSet.add(ch);
         }
+        if (charsSet.size() < chars.length){
+            return true;
+        }
+
         return false;
     }
-
-    public static boolean hasDuplicateChars(String string) {   //строка без повторений 1000 6мс, 3000 8мс, 8195 19мс, 30000 184мс, 65536 809мс
-        char[] chars = new char[string.length()];
-        for (int i = 0; i < chars.length; i++) {
-            chars[i] = string.charAt(i);
-        }
-        for (int i = 0; i < chars.length; i++) {
-            for (int j = i + 1; j < chars.length; j++){
-                if(chars[i] == chars[j]){
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    //думаю, на шарпе можно было бы написать что-то вот такое, ибо каждая строка - уже массив символов
-            //но увы
-    /* public static boolean hasDuplicateSymbolsForeach(String string) {
-        for (char ch : string){
-            (укоротить строку)
-            for (char cha : string){
-                if (ch == cha){
-                    return true;
-                }
-            }
-        }
-        return false;
-    } */
-
-
 }
